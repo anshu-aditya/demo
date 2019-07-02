71f3be6fb64a15ddf7657da770c2884f66e16b9a
Anshu <anshuaditya@outlook.com>
2019-07-03 00:21:11 +0530

    first commmit

---
+++ search.php
@@ -0,0 +1,18 @@
+<?php 
+
+$link = mysqli_connect('localhost', 'root', '', 'ajax');
+if(!$link){
+    die("DataBase Connection Failed!");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ search3.php
@@ -0,0 +1,16 @@
+<?php 
+
+include("db.php");
+
+
+//if($connection) {
+//
+//echo "Yes it is";
+//
+//}
+
+$search = $_POST['search'];
+
+echo $search;
+
+?>
